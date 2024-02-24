b77cbf7a29236eaeca0a3f0b5f48bff2853bcca8
Douglas Poma <douglaspoma@yahoo.com>
2024-02-24 08:54:23 -0300

    feat: add domain service to calculate distance

--- src/application/usecases/GetRide.ts
+++ src/application/usecases/GetRide.ts
@@ -1,3 +1,5 @@
+import { Coord } from '../../domain/Coord'
+import { DistanceCalculator } from '../../domain/DistanceCalculator'
 import { Logger } from '../logger/Logger'
 import { PositionRepository } from '../repositories/PositionRepository'
 import { RideRepository } from '../repositories/RideRepository'
@@ -17,23 +19,12 @@ export class GetRide {
     let distance = 0
     for (const [index, position] of positions.entries()) {
       if (!positions[index + 1]) break
-      const from = { lat: position.coord.lat, long: position.coord.long }
-      const to = {
-        lat: positions[index + 1].coord.lat,
-        long: positions[index + 1].coord.long,
-      }
-      const earthRadius = 6371
-      const degreesToRadians = Math.PI / 180
-      const deltaLat = (to.lat - from.lat) * degreesToRadians
-      const deltaLon = (to.long - from.long) * degreesToRadians
-      const a =
-        Math.sin(deltaLat / 2) * Math.sin(deltaLat / 2) +
-        Math.cos(from.lat * degreesToRadians) *
-          Math.cos(to.lat * degreesToRadians) *
-          Math.sin(deltaLon / 2) *
-          Math.sin(deltaLon / 2)
-      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
-      distance += Math.round(earthRadius * c)
+      const from = new Coord(position.coord.lat, position.coord.long)
+      const to = new Coord(
+        positions[index + 1].coord.lat,
+        positions[index + 1].coord.long,
+      )
+      distance += DistanceCalculator.calculate(from, to)
     }
     return {
       rideId: ride.rideId,
